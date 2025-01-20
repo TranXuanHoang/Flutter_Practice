87749e766b49c6b9d2331bf7482e9854221985a2
TranXuanHoang <hoangtx.ict@gmail.com>
2025-01-20 09:53:27 +0900

    Add an animated list of favorites to `awesome_namer` app

--- awesome_namer/lib/main.dart
+++ awesome_namer/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -44,21 +43,45 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var history = <WordPair>[];
+
+  /// A GlobalKey that is presumably associated with an AnimatedList widget
+  GlobalKey? historyListKey;
 
   void getNext() {
+    // Insert the current randomly generated WordPair to history
+    history.insert(0, current);
+
+    // Insert a new item at the beginning of an AnimatedList if the historyListKey
+    // is associated with an AnimatedList and its state is available
+    var animatedList = historyListKey?.currentState as AnimatedListState?;
+    animatedList?.insertItem(0);
+
+    // Generate a random WordPair
     current = WordPair.random();
+
     notifyListeners();
   }
 
   var favorites = <WordPair>[];
 
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
+  void toggleFavorite([WordPair? pair]) {
+    pair = pair ?? current;
+    if (favorites.contains(pair)) {
+      favorites.remove(pair);
     } else {
-      favorites.add(current);
+      favorites.add(pair);
     }
     notifyListeners();
+  }
+
+  void removeFavorite(WordPair pair) {
+    favorites.remove(pair);
+    notifyListeners();
+  }
+
+  bool isFavorite(WordPair pair) {
+    return favorites.contains(pair);
   }
 }
 
@@ -145,6 +168,10 @@ class GeneratorPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Expanded(
+            flex: 3,
+            child: HistoryListView(),
+          ),
           BigCard(pair: pair),
           SizedBox(height: 20),
           Row(
@@ -165,6 +192,7 @@ class GeneratorPage extends StatelessWidget {
                   child: Text('Next')),
             ],
           ),
+          Spacer(flex: 2)
         ],
       ),
     );
@@ -223,6 +251,12 @@ class FavoritesPage extends StatelessWidget {
       itemBuilder: (context, index) => Card(
         elevation: 5,
         child: ListTile(
+          leading: IconButton(
+            onPressed: () {
+              appState.removeFavorite(favorites[index]);
+            },
+            icon: Icon(Icons.delete),
+          ),
           title: Text(
             favorites[index].asLowerCase,
           ),
@@ -243,6 +277,65 @@ class FavoritesPage extends StatelessWidget {
           ),
           hoverColor: Theme.of(context).colorScheme.onSecondary,
         ),
+      ),
+    );
+  }
+}
+
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({super.key});
+
+  @override
+  State<HistoryListView> createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<HistoryListView> {
+  /// Needed so that [MyAppState] can tell [AnimatedList] below to animate
+  final _key = GlobalKey();
+
+  /// Used to "fade out" the history items at the top, to suggest continuation
+  static const Gradient _maskingGradient = LinearGradient(
+    // This gradient goes from fully transparent to fully opaque black
+    colors: [Colors.transparent, Colors.black],
+    // ... from the top (transparent) to half (0.5) of the way to the bottom
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>();
+    appState.historyListKey = _key;
+    final history = appState.history;
+
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskingGradient.createShader(bounds),
+      // This blend mode takes the opacity of the shader (i.e. our gradient)
+      // and applies it to the destination (i.e. our animated list).
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: appState.historyListKey,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: history.length,
+        itemBuilder: (context, index, animation) {
+          final pair = history[index];
+          return SizeTransition(
+              sizeFactor: animation,
+              child: Center(
+                child: TextButton.icon(
+                  icon: appState.isFavorite(pair)
+                      ? Icon(Icons.favorite, size: 12)
+                      : SizedBox(),
+                  label: Text(
+                    pair.asLowerCase,
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                  onPressed: () => appState.toggleFavorite(pair),
+                ),
+              ));
+        },
       ),
     );
   }
